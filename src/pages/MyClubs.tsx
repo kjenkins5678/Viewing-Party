04519cb96989b2657ee65c7c74d0ac55d8e701b2
kjenkins5678 <kajenkins9@gmail.com>
2020-04-21 16:11:41 -0400

    added pages: my clubs, my page, search

--- src/pages/MyClubs.tsx
+++ src/pages/MyClubs.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
-import './Tab2.css';
+import './MyClubs.css';
 
-const Tab2: React.FC = () => {
+const MyClubs: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle> My Clubs</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
+            <IonTitle size="large">My Clubs</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+        <ExploreContainer name="My Clubs" />
       </IonContent>
     </IonPage>
   );
 };
 
-export default Tab2;
+export default MyClubs;
--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+import './Search.css';
 
-const Tab1: React.FC = () => {
+const Search: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Search Movies or Shows</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle size="large">Search Movies or Shows</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <ExploreContainer name="Search Movies or Shows" />
       </IonContent>
     </IonPage>
   );
 };
 
-export default Tab1;
+export default Search;
