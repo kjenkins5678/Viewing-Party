2d356c132f43f61d337d7ad283f2bebff2c960f7
raquelacevedo <raquel.e.acevedo@gmail.com>
2020-04-25 10:46:51 -0400

    initial changes

---
+++ src/pages/MovieService.ts
@@ -0,0 +1,4 @@
+import React from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import ExploreContainer from '../components/ExploreContainer';
+import { API_URL, API_KEY } from '../config'
