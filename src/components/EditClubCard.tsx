2781228e5a416e27515aa5e7e842633ea32c75a0
kjenkins5678 <kajenkins9@gmail.com>
2020-04-28 17:27:29 -0400

    split card component into edit card and card

---
+++ src/components/EditClubCard.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { IonCard, IonCardHeader, IonItem, IonLabel, IonButton, IonIcon } from '@ionic/react';
+import { hammer, trash } from 'ionicons/icons';
+
+const EditClubCard: React.FC = () => {
+  return (
+      <IonCard className="club-card">
+        <IonCardHeader>
+          <IonLabel>
+                <h2>Club Name Here</h2>
+          </IonLabel>
+        </IonCardHeader>
+        <IonItem routerLink={`/club/club_id_here`}>
+          <IonButton>
+            <IonIcon slot="icon-only" icon={hammer} />
+          </IonButton>
+        </IonItem>
+        <IonItem routerLink={`/club/delete/club_id_here`}>
+          <IonButton>
+            <IonIcon icon={trash} />
+          </IonButton>
+        </IonItem>
+
+      </IonCard>
+  );
+};
+
+export default EditClubCard;
