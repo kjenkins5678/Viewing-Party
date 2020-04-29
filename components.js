46197a5033dcef45c69d2547777306b708612fb5
Madison Schrews <maddyschrews@gmail.com>
2020-04-29 18:24:37 -0400

    more updates to picker component

--- components.js
+++ components.js
@@ -34,3 +34,110 @@ export const TabsExample: React.FC = () => (
 );
 
 // the following is a picker in ionic. 
+
+import React from "react";
+import { PickerColumn } from "@ionic/core";
+import { IonPicker } from "@ionic/react";
+
+/**
+ * the component has three properties that are defined in this 
+ * interface since we are using typescript
+ */
+interface _Props {
+  isOpen : boolean
+  onSave : Function
+  onCancel : Function
+}
+
+const MyPicker: React.FC<_Props> = ({onSave, onCancel, isOpen}) => { 
+  return <div></div>
+}
+
+const DayColumn = {
+  name: "Day",
+  options: [
+    { text: "Monday", value: "Monday" },
+    { text: "Tuesday", value: "Tuesday" },
+    { text: "Wednesday", value: "Wednesday" },
+    { text: "Thursday", value: "Thursday" },
+    { text: "Friday", value: "Friday" }
+  ]
+} as PickerColumn;;
+
+const SessionTimeColumn = {
+  name: "SessionTime",
+  options: [
+    { text: "Morning 8a - 12p", value: "Morning" },
+    { text: "Afteroon 1p - 5p", value: "Afteroon" }
+  ]
+} as PickerColumn;
+
+return (
+  <div>
+    <IonPicker
+      isOpen={isOpen}
+      columns={[DayColumn, SessionTimeColumn]}
+      buttons={[
+        {
+          text: "Cancel",
+          role: "cancel",
+          handler: value => {
+            onCancel()
+          }
+        },
+        {
+          text: "Confirm",
+          handler: value => {
+            onSave(value)
+          }
+        }
+      ]}
+    ></IonPicker>
+  </div>
+);
+
+import React, { useState } from "react";
+import "./Home.css";
+import MyPicker from "../src/components.js";
+
+export interface ISessionTime {
+  weekday: string;
+  period: string;
+}
+
+const [pickerIsOpen, setPickerIsOpen] = useState(false);
+const [sessionTime, setSessionTime] = useState<ISessionTime | undefined>(
+    undefined
+  );
+
+  <IonItem>
+  <IonButton onClick={() => { setPickerIsOpen(true); }} >
+    Select Session
+  </IonButton>
+  <IonButton onClick={() => { setSessionTime(undefined); }}>
+    Clear Session
+  </IonButton>
+</IonItem>
+
+<IonItem onClick={() => { setPickerIsOpen(true); }} >
+  {sessionTime ? (
+    <IonLabel>
+      {sessionTime?.weekday} - {sessionTime?.period}
+    </IonLabel>
+  ) : (
+    <IonLabel className="placeHolder">Please Select Session</IonLabel>
+  )}
+</IonItem>
+
+<MyPicker
+  isOpen={pickerIsOpen}
+  onCancel={() => {
+    setPickerIsOpen(false);
+  }}
+  onSave={(_value: any) => {
+    console.log(_value);
+    let { Day, SessionTime } = _value;
+    setSessionTime({ weekday: Day.value, period: SessionTime.value });
+    setPickerIsOpen(false);
+  }}
+/>
