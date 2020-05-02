7b363de7e4def0499c5171f8d5508db9c449d1b0
BrianMcL <bmclaugh28@yahoo.com>
2020-05-01 22:46:27 -0400

    jawsdb connection when available

--- models/mySqlConnection.js
+++ models/mySqlConnection.js
@@ -1,12 +1,14 @@
 const mysql = require('mysql2');
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Mysql000!', 
-  database: 'viewing_party_db'
+
+var connection; 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Mysql000!', 
+    database: 'viewing_party_db'
 });
-//console.log (connection); 
 
 module.exports = connection; 
-
-
